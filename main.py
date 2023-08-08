efa5a4800b256a1fb732a168cac2d6fc05590583
itsmmdoha <getmonazirnow@gmail.com>
2023-08-08 21:49:28 +0600

    Idea vomit

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ class File:
     def __init__(self,url,numberOfThreads):
         File.url = url
         File.numberOfThreads = numberOfThreads
+    def chunkSize(self):
+        pass
+
+    def getFileName(self):
+        pass
 
     def getChunk(self,startByte,endByte):
         pass
